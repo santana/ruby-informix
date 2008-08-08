b56f2abdcac691a6195498c39bf7fc642b668dd6
Gerardo Santana <gerardo.santana@gmail.com>
2008-08-07 19:43:42 -0500

    Bump release number

--- lib/informix.rb
+++ lib/informix.rb
@@ -32,7 +32,7 @@ require 'informix/seqcursor'
 require 'informix/scrollcursor'
 
 module Informix
-  VERSION = "0.7.0"
+  VERSION = "0.7.2"
   VERSION.freeze
 
   # Shortcut to create a +Database+ object connected to +dbname+ as
