2f9510f95b002a5a42d535ad5f28f6e5ea06f438
Gerardo Santana <gerardo.santana@gmail.com>
2012-04-10 07:10:51 -0500

    Replaces minitest with test-unit

--- test/ifx_all.rb
+++ test/ifx_all.rb
@@ -2,6 +2,7 @@ testdir = File.expand_path(File.dirname(__FILE__))
 $LOAD_PATH.unshift File.join(testdir, "..")
 
 require 'informix'
+gem 'test-unit'
 require 'test/unit'
 
 class IfxAll
@@ -23,9 +24,7 @@ if __FILE__ == $0
 ruby #{$0} database [username password]"
     exit 1
   end
-  require 'test/unit/ui/console/testrunner'
   Dir.glob(File.join(testdir, "ifx_test*.rb")).each do |testcase|
     require "#{testcase}"
   end
-  Test::Unit::UI::Console::TestRunner.run(IfxAll)
 end
